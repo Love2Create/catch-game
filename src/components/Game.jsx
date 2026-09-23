e98b53e92499b1150ac2592ed4d9f3b27810d511
Vicky Lu <vl@MacBookPro.lan>
2026-09-22 21:29:39 -0400

    hero follow mouse

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,13 +1,24 @@
-import {useState} from 'react';
+import {useState, useRef} from 'react';
 import './Game.css';
 import Hero from './Hero';
 
 const Game = () => {
 
-    const [heroPos, setHeroPos] = useState({x:20, y:50})
+    const [heroPos, setHeroPos] = useState({x:20, y:50});
+
+    const gameRef = useRef(null);
+    const yOffset = 5;
+
+    const onMove = (e) => {
+        const bounce = gameRef.current.getBoundingClientRect();
+        const pointerX = ((e.clientX - bounce.left) / bounce.width) * 100;
+        const pointerY = ((e.clientY / bounce.height) * 100) - yOffset;
+
+        setHeroPos({x:pointerX, y:pointerY});
+    }
 
     return(
-        <div className="game">
+        <div className="game" onMouseMove={onMove} ref={gameRef}>
             <Hero pos={heroPos}/>
         </div>
     )
